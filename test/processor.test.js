5904e0cc6eb5bff1cff2ec7bc8e0de5a8da3d5b7
Romain Billot <206266704@cegepsherbrooke.qc.ca>
2024-07-09 10:57:45 -0400

    requis5 fail

--- test/processor.test.js
+++ test/processor.test.js
@@ -24,4 +24,9 @@ describe("transmission processor", function () {
 		expect(typeof result.id).toEqual("number");
 	});
 
+	test("returns rawData in object", function () {
+		let result = processor("9701::<489584872710>");
+		expect(result.rawData).not.toEqual(undefined);
+	});
+
 });
